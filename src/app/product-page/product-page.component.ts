a90235fd95f7b1662ffcb08b3dca2fbae268911e
Jethro Moey <jmse97@gmail.com>
2025-06-17 14:54:07 +0800

    feat(service): Added product.service

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -10,6 +10,7 @@ import { debounceTime, distinctUntilChanged } from 'rxjs';
 import { PaginationComponent } from '../pagination/pagination.component';
 import { ProductCardListComponent } from '../product-card-list/product-card-list.component';
 import { Product } from '../models/product';
+import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-product-page',
@@ -22,67 +23,12 @@ export class ProductPageComponent {
 
   private productService = inject(ProductService);
 
-  private destroyRef = inject(DestroyRef);
-
-  readonly searchControl = new FormControl<string | undefined>(undefined, {
-    nonNullable: true,
-  });
-
-  readonly productName = toSignal(
-    this.searchControl.valueChanges.pipe(
-      debounceTime(500),
-      distinctUntilChanged(),
-      takeUntilDestroyed(this.destroyRef)
-    ),
-    {
-      initialValue: undefined,
-    }
-  );
-
-  readonly pageIndex = signal(1);
-
-  readonly pageSize = signal(5);
-
-  private readonly data = rxResource({
-    request: () => ({
-      name: this.productName(),
-      pageIndex: this.pageIndex(),
-      pageSize: this.pageSize(),
-    }),
-    defaultValue: { data: [], count: 0 },
-    loader: ({ request }) => {
-      const { name, pageIndex, pageSize } = request;
-      return this.productService.getList(name, pageIndex, pageSize);
-    },
-  });
-
-  readonly totalCount = computed(() => {
-    const { count } = this.data.value();
-    return count;
-  });
-
-  readonly products = computed(() => {
-    const { data } = this.data.value();
-    return data;
-  });
+  products: Product[] = [];
 
   ngOnInit(): void {
-    this.searchControl.valueChanges.subscribe((value) => console.log(value));
-  }
-
-  onEdit(product: Product): void {
-    this.router.navigate(['product', 'form', product.id]);
-  }
-
-  onView(product: Product): void {
-    this.router.navigate(['product', 'view', product.id]);
-  }
-
-  onAdd(): void {
-    this.router.navigate(['product', 'new']);
-  }
-
-  onRemove({ id }: Product): void {
-    this.productService.remove(id).subscribe(() => this.pageIndex.set(1));
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.productService = new ProductService();
+    this.products = this.productService.getList();
   }
 }
---
+++ src/app/services/product.service.ts
@@ -0,0 +1,115 @@
+import { Injectable, OnInit } from '@angular/core';
+import { Product } from '../models/product';
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductService {
+  private _data: Product[] = [
+    new Product({
+      id: '1',
+      name: '書籍 A',
+      authors: ['作者甲', '作者乙', '作者丙'],
+      company: '博碩文化',
+      isShow: true,
+      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+      createDate: new Date('2025/4/9'),
+      price: 10000,
+    }),
+    new Product({
+      id: '2',
+      name: '書籍 B',
+      authors: ['作者甲', '作者乙', '作者丙'],
+      company: '博碩文化',
+      isShow: true,
+      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+      createDate: new Date('2025/4/9'),
+      price: 10000,
+    }),
+    new Product({
+      id: '3',
+      name: '書籍 C',
+      authors: ['作者甲', '作者乙', '作者丙'],
+      company: '博碩文化',
+      isShow: true,
+      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+      createDate: new Date('2025/4/9'),
+      price: 10000,
+    }),
+    new Product({
+      id: '4',
+      name: '書籍 D',
+      authors: ['作者甲', '作者乙', '作者丙'],
+      company: '博碩文化',
+      isShow: true,
+      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+      createDate: new Date('2025/4/9'),
+      price: 10000,
+    }),
+    new Product({
+      id: '5',
+      name: '書籍 E',
+      authors: ['作者甲', '作者乙', '作者丙'],
+      company: '博碩文化',
+      isShow: true,
+      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+      createDate: new Date('2025/4/9'),
+      price: 10000,
+    }),
+    new Product({
+      id: '6',
+      name: '書籍 F',
+      authors: ['作者甲', '作者乙', '作者丙'],
+      company: '博碩文化',
+      isShow: true,
+      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+      createDate: new Date('2025/4/9'),
+      price: 10000,
+    }),
+    new Product({
+      id: '7',
+      name: '書籍 G',
+      authors: ['作者甲', '作者乙', '作者丙'],
+      company: '博碩文化',
+      isShow: true,
+      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+      createDate: new Date('2025/4/9'),
+      price: 10000,
+    }),
+    new Product({
+      id: '8',
+      name: '書籍 H',
+      authors: ['作者甲', '作者乙', '作者丙'],
+      company: '博碩文化',
+      isShow: true,
+      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+      createDate: new Date('2025/4/9'),
+      price: 10000,
+    }),
+    new Product({
+      id: '9',
+      name: '書籍 I',
+      authors: ['作者甲', '作者乙', '作者丙'],
+      company: '博碩文化',
+      isShow: true,
+      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+      createDate: new Date('2025/4/9'),
+      price: 10000,
+    }),
+    new Product({
+      id: '10',
+      name: '書籍 J',
+      authors: ['作者甲', '作者乙', '作者丙'],
+      company: '博碩文化',
+      isShow: true,
+      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+      createDate: new Date('2025/4/9'),
+      price: 10000,
+    }),
+  ];
+
+  getList(): Product[] {
+    return this._data;
+  }
+}
