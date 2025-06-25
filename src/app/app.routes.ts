b4662f0f4629f7523e62c3e96af2e8ff2b79929b
Jethro Moey <jmse97@gmail.com>
2025-06-25 10:59:02 +0800

    feat(router): Routed product-page and cart-page onto links in nav-bar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
+import { CartPageComponent } from './cart-page/cart-page.component';
 import { Routes } from '@angular/router';
+import { ProductPageComponent } from './product-page/product-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'products', component: ProductPageComponent },
+  { path: 'cart', component: CartPageComponent },
+];
