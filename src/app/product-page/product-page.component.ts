e0bf6eafb3c61de1955acebcff2c0161c2a7d886
Jethro Moey <jmse97@gmail.com>
2025-06-25 15:24:58 +0800

    feat(product-page): Added product-card-list componentinto product-page

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { ProductCardListComponent } from '../product-card-list/product-card-list.component';
 
 @Component({
   selector: 'app-product-page',
-  imports: [],
+  imports: [ProductCardListComponent],
   templateUrl: './product-page.component.html',
-  styleUrl: './product-page.component.scss'
+  styleUrl: './product-page.component.scss',
 })
-export class ProductPageComponent {
-
-}
+export class ProductPageComponent {}
