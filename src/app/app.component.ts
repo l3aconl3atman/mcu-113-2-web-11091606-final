94fadac4ae6f48e73bfdddeb565417e7743b3f7d
Jethro Moey <jmse97@gmail.com>
2025-08-04 17:31:45 +0800

    Create and stylize toolbar in product-page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
+import { ProductPageComponent } from './product-page/product-page.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, ProductPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
