c6b4e23ed88f5facbbdd93728d8656ede29de7c4
Jethro Moey <jmse97@gmail.com>
2025-08-17 22:45:04 +0800

    Created Cart-Page Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
-import { ProductPageComponent } from './product-page/product-page.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, ProductPageComponent],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
