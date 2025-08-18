b15938ecaf9efaf273886f3767623721860a8b7e
Jethro Moey <jmse97@gmail.com>
2025-08-18 17:50:41 +0800

    Using Dependency injection instead of new instance to get product services

--- src/app/product-detail-page/product-detail-page.component.ts
+++ src/app/product-detail-page/product-detail-page.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-product-detail-page',
   imports: [],
   templateUrl: './product-detail-page.component.html',
-  styleUrl: './product-detail-page.component.scss'
+  styleUrl: './product-detail-page.component.scss',
 })
 export class ProductDetailPageComponent {
-
+  private productService = inject(ProductService);
 }
