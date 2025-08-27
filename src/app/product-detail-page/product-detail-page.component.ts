e83b96eea89696ebefdafa9e7d6bf37a0875dc33
Jethro Moey <jmse97@gmail.com>
2025-08-27 23:18:19 +0800

    Make changes to Product Detail Page

--- src/app/product-detail-page/product-detail-page.component.ts
+++ src/app/product-detail-page/product-detail-page.component.ts
@@ -1,12 +1,28 @@
 import { Component, inject } from '@angular/core';
-import { ProductService } from '../services/product.service';
+import { Product } from '../models/product';
+import { CurrencyPipe } from '@angular/common';
+
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-detail-page',
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './product-detail-page.component.html',
   styleUrl: './product-detail-page.component.scss',
 })
 export class ProductDetailPageComponent {
-  private productService = inject(ProductService);
+  product = new Product({
+    name: '書籍 Z',
+    authors: ['作者甲', '作者乙', '作者丙'],
+    company: '博碩文化',
+    createDate: new Date('2025/8/5'),
+    photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+    price: 10000,
+  });
+
+  readonly router = inject(Router);
+
+  onBack(): void {
+    this.router.navigate(['products']);
+  }
 }
